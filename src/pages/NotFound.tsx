65aa5b06444c64cb2356c2f6fe69b5ab2ff590b6
veil <shenosghosting@gmail.com>
2026-04-26 00:38:37 +0200

    Update NotFound component message and link

--- src/pages/NotFound.tsx
+++ src/pages/NotFound.tsx
@@ -5,16 +5,16 @@ const NotFound = () => {
   const location = useLocation();
 
   useEffect(() => {
-    console.error("404 Error: User attempted to access non-existent route:", location.pathname);
+    console.error("404 Error: User attempted to access some non-existent route:", location.pathname);
   }, [location.pathname]);
 
   return (
     <div className="flex min-h-screen items-center justify-center bg-muted">
       <div className="text-center">
-        <h1 className="mb-4 text-4xl font-bold">404</h1>
-        <p className="mb-4 text-xl text-muted-foreground">Oops! Page not found</p>
+        <h1 className="mb-4 text-4xl font-bold">What are you doing here?</h1>
+        <p className="mb-4 text-xl text-muted-foreground">There is nothing here! What were you trying to find?</p>
         <a href="/" className="text-primary underline hover:text-primary/90">
-          Return to Home
+          Look at my bio
         </a>
       </div>
     </div>
